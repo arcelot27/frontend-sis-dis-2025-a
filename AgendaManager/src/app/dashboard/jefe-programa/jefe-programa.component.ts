e5ff0db5e212ecda9af945c4de928c953577db30
arcelot27 <arctamua@gmail.com>
2025-04-15 15:12:19 -0500

    feature [H3]: ✨ Generate componets

---
+++ AgendaManager/src/app/dashboard/jefe-programa/jefe-programa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jefe-programa',
+  imports: [],
+  templateUrl: './jefe-programa.component.html',
+  styleUrl: './jefe-programa.component.css'
+})
+export class JefeProgramaComponent {
+
+}
