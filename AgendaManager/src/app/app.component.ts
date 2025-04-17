1bb8f74f02339f083d2f1c6c40c20dfdcfa55b10
arcelot27 <arctamua@gmail.com>
2025-04-17 12:22:16 -0500

    ✨  feature [H3]: Generate components, style and functionality

--- AgendaManager/src/app/app.component.ts
+++ AgendaManager/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { FormularioComponent } from './formulario/formulario.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormularioComponent],
+  imports: [
+    CommonModule,
+    FormularioComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
